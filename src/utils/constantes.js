0e5ed0412ca7f9791cf4bbe5dac0ffeb793bb2f2
Gustavo Iglesias <gustavo.iglesias40@gmail.com>
2024-01-03 16:30:57 -0300

    urlservidor

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,3 +1,3 @@
-const NOMBRE_BB_DD="elsoberbio";
-const BASE_URL = 'https://api.elsoberbio.unsada.edu.ar/api';
+const NOMBRE_BB_DD="pampadelindio";
+const BASE_URL = 'https://apipampadelindio.cfl401areco.ar/api';
 export{NOMBRE_BB_DD,BASE_URL}
