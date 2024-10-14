7069aa4b9973d80f740f6aa97845a1d0f5d6eb51
Gustavo Iglesias <gustavo.iglesias40@gmail.com>
2024-10-14 13:25:56 -0300

    cambio de api

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,3 +1,3 @@
 const NOMBRE_BB_DD="triplefrontera";
-const BASE_URL = "https://areco.gob.ar:9535/api"//'https://apipampadelindio.cfl401areco.ar/api';
+const BASE_URL = 'https://api.triplefrontera.unsada.edu.ar/api';
 export{NOMBRE_BB_DD,BASE_URL}
